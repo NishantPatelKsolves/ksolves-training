d8a9b59201a033b4ba5d422d812b084ff475989e
NishantPatelKsolves <nishant.patel@ksolves.com>
2024-09-28 19:02:30 +0530

    helper utility : standardized api response

---
+++ BlogApp/backend/src/utils/strdApiResponse.js
@@ -0,0 +1,9 @@
+class StrdApiResponse {
+    constructor(statusCode, data, message = 'Success') {
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message < 400;
+    }
+}
+
+export default StrdApiResponse;
