7668e9a2639ac13d110ad9d6193c966b6a0ddcf7
NishantPatelKsolves <nishant.patel@ksolves.com>
2024-09-28 16:03:57 +0530

    configure cors in the project

--- BlogApp/backend/src/app.js
+++ BlogApp/backend/src/app.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
+
+//only after app is created, we inject middlewares
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credential:true
+}))
 
 app.get('/',(req,res)=>{
     res.send('Home route');
