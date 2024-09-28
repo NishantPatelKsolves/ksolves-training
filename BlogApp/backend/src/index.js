11e3cb7ec75e66a85c81954df2119e25d3ee3c95
NishantPatelKsolves <nishant.patel@ksolves.com>
2024-09-28 15:57:09 +0530

    configure dotenv in the project

--- BlogApp/backend/src/index.js
+++ BlogApp/backend/src/index.js
@@ -1,6 +1,18 @@
 //console.log('Hello World!')
 
 import { app } from "./app.js";
-app.listen(9000,()=>{
-    console.log(`Server active at: http://localhost:9000` )
+import dotenv from 'dotenv';
+const resultEnvLoading= dotenv.config({
+    path: './.env'
+})
+if (resultEnvLoading.error) {
+    console.log(`Error loading environment variables`);
+    throw resultEnvLoading.error;
+} else {
+    
+}
+
+const port = process.env.PORT || 9001
+app.listen(port ,()=>{
+    console.log(`Server active at: http://localhost:${port}` )
 })
