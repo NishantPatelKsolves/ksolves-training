7c98dbca959d1b1717383f6733b963782d42799f
NishantPatelKsolves <nishant.patel@ksolves.com>
2024-09-29 22:21:21 +0530

    hash passwords before saving to database

--- BlogApp/backend/src/models/user.models.js
+++ BlogApp/backend/src/models/user.models.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema(
     {
@@ -47,5 +48,21 @@ const userSchema = new mongoose.Schema(
     },
     { timestamps: true } //when true, document automatically gets two fields: createdAt and updatedAt
 );
+
+userSchema.pre('save', async function (params) {
+    if (!this.modified('password')) return next(); //if the paswword is not modified don't hash it in that case; first time the password is not modified so below code runs.
+    this.password = bcrypt.hash(this.password, 10); //hash 10 rounds
+    next();
+});
+
+/**
+ * checks if password matches tge one stored in DB
+ * @param password : the clear text password by user
+ * - bcrypt automatically figures out hashing algo and number of rounds, etc,so no need to specify when matching passwords
+ * @returns boolean, whether password matches or fails
+ */
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
 
 export const User = mongoose.model('User', userSchema);
