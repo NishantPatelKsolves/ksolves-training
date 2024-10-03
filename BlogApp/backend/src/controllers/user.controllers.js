fc16e31b65066a1e417189aa65bd24a0cf34637d
NishantPatelKsolves <nishant.patel@ksolves.com>
2024-10-03 12:09:20 +0530

    login controller setup

--- BlogApp/backend/src/controllers/user.controllers.js
+++ BlogApp/backend/src/controllers/user.controllers.js
@@ -21,7 +21,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
     //check if user already exist in our DB
     const existingUser = await User.findOne({
-        $or: [{ username }, { email }], //search based on username and email, if anyone mathes, user is declared 'existing'
+        $or: [{ username }, { email }], //search based on username and email, if anyone matches, user is declared 'existing'
     });
     if (existingUser) {
         throw new StrdApiErrorResponse(
@@ -119,4 +119,98 @@ const registerUser = asyncHandler(async (req, res) => {
         );
     }
 });
+
+const generateAccessTokenAndRefreshToken = async (userId) => {
+    try {
+        const user = await new User.findById(userId);
+        //check if user existes
+        if (!user) {
+            throw new StrdApiErrorResponse(401, 'User does not exist');
+            console.log(`User with id: ${userId} does not exist`);
+            return null;
+        }
+
+        //generate tokens
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        //set refresh token on User in db
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false });
+
+        return { accessToken, refreshToken };
+    } catch (error) {
+        console.log('Error generating token', error);
+        throw new StrdApiErrorResponse(
+            500,
+            'Something went wrong while generating access and refresh token'
+        );
+    }
+};
+
+const loginUser = asyncHandler(async (req, res) => {
+    //1. get data from body
+    const { email, username, password } = req.body;
+    //validate data
+    if (!email || !username || !password) {
+        throw new StrdApiErrorResponse(
+            400,
+            'Email, Username and Password are mandatory'
+        );
+    }
+
+    //2. check if user exist, based on id and username
+    const user = await User.findOne({
+        $or: [{ username }, { email }],
+    });
+    if (!user) {
+        console.log(`User with this email/username does not exist`);
+        throw new StrdApiErrorResponse(
+            401,
+            'User with this email/username does not exist'
+        );
+    }
+
+    //3. once 'user' found -> validate password
+    const validPassword = await user.isPasswordCorrect(password);
+    if (!validPassword) {
+        console.log('Password authentication failed');
+        throw new StrdApiErrorResponse(400, 'Incorrect password');
+    }
+
+    //4. if password is correct generate tokens(both)
+    const { accessToken, refreshToken } =
+        await generateAccessTokenAndRefreshToken(user._id); // this generates the tokens and also saves them on DB
+
+    //5. for fail-safe approach: new db query to fetch the user with updated credentials
+    const loggedInUser = await User.findById(user._id).select(
+        '-password -refreshToken' //exclude fetching password and refreshToken
+    );
+    if (!loggedInUser) {
+        console.log('Something went wrong with accessing logged in user');
+        throw new StrdApiErrorResponse(
+            500,
+            'Something went wrong with accessing logged in user'
+        );
+    }
+
+    //6. send logged-in user details  to frontend
+    const options = {
+        httpOnly: true, //make cookie not-modifyable at client end
+        secure: process.env.NODE_ENV === 'production', // makes 'true' when env is prod, else 'false' is okay for testing
+    };
+
+    return res
+        .status(200)
+        .cookie('accessToken', accessToken, options)
+        .cookie('refreshToken', refreshToken, options)
+        .json(
+            new StrdApiErrorResponse(
+                200,
+                loggedInUser,
+                'User loggen in successfully'
+            )
+        );
+});
+
 export { registerUser };
